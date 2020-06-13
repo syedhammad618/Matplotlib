f5324db8ca10869023390f0848f3691b3b5ff94d
Hammad <syedhammad618@gmail.com>
2020-06-13 21:10:49 +0500

    all graph are up to date

--- matplot.py
+++ matplot.py
@@ -83,26 +83,70 @@
 
 # gas price data graph
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+
+# plt.figure(figsize=(10,10))
+# gas_data=pd.read_csv('gas_price.csv')
+# plt.plot(gas_data.Year,gas_data.Japan,marker=".")
+# plt.plot(gas_data.Year,gas_data.Canada ,marker=".")
+# plt.plot(gas_data.Year,gas_data.Germany ,marker=".")
+# # for country in gas_data:
+# #     if country != 'Year':
+# #         plt.plot(gas_data.Year,gas_data[country],marker=".",label=country)
+        
+# plt.title("Gas Prices Over Time In (USD)",color="#1ec0e8",size=20,fontweight="bold")
+# plt.xlabel("Years",color="#1eb2e8",size=15)
+# plt.ylabel("Gas Price",color="#1eb2e8",size=15)
+# plt.legend(["japan","Canada","Germany"])
+# plt.grid()
+# plt.xticks(gas_data.Year[::3])
+# plt.show()
+
+
+
+# histogram 
+
+# import matplotlib.pyplot as plt 
+# import numpy as np
+# import pandas as pd
+# bins=[10,20,30,40,50,60,70,80,90,100]
+# fifa=pd.read_csv('fifa_data.csv')
+# plt.title("Distribution Of Player Skill")
+# plt.xlabel("Player Skills")
+# plt.ylabel("Number Of Player")
+# plt.hist(fifa.Overall,bins=bins,rwidth=0.95)
+# plt.show()
+
+
+# Pie Chart
+# import matplotlib.pyplot as plt 
+# import numpy as np
+# import pandas as pd
+# fifa=pd.read_csv('fifa_data.csv')
+# labels=["Left","Right"]
+# explode=(0.1,0)
+
+# left=fifa.loc[fifa['Preferred Foot']=="Left"].count()[0]
+# right=fifa.loc[fifa['Preferred Foot']=="Right"].count()[0]
+# plt.title("Foot Preference Of Fifa Players")
+# plt.pie([left,right],labels=labels,shadow=True,autopct="%1.1f%%",explode=explode)
+# plt.show()
+
+
+
+import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
 
-plt.figure(figsize=(10,10))
-gas_data=pd.read_csv('gas_price.csv')
-plt.plot(gas_data.Year,gas_data.Japan,marker=".")
-plt.plot(gas_data.Year,gas_data.Canada ,marker=".")
-plt.plot(gas_data.Year,gas_data.Germany ,marker=".")
-# for country in gas_data:
-#     if country != 'Year':
-#         plt.plot(gas_data.Year,gas_data[country],marker=".",label=country)
-        
-plt.title("Gas Prices Over Time In (USD)",color="#1ec0e8",size=20,fontweight="bold")
-plt.xlabel("Years",color="#1eb2e8",size=15)
-plt.ylabel("Gas Price",color="#1eb2e8",size=15)
-plt.legend(["japan","Canada","Germany"])
-plt.grid()
-plt.xticks(gas_data.Year[::3])
+fifa=pd.read_csv('fifa_data.csv')
+labels=["FC Barcelona","Real Madrid"]
+barcelona=fifa.loc[(fifa.Club=="FC Barcelona")]["Overall"]
+madrid=fifa.loc[(fifa.Club=="Real Madrid")]["Overall"]
+plt.title("Soccer Team Comparison")
+plt.ylabel("Over All Rating")
+plt.boxplot([barcelona,madrid],labels=labels)
 plt.show()
-
-
-
+print(barcelona)
+print(madrid)
